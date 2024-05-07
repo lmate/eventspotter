ec68c19587c221440c5ffd50e247345f8d185a5b
Mate Lenkey <9825717+lmate@users.noreply.github.com>
2024-05-07 14:39:10 +0200

    Replace google maps with open street map

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:client/models/event_model.dart';
 import 'package:client/services/event_service.dart';
+import 'package:client/map.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,9 +14,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-  final LatLng _center = const LatLng(47.497913, 19.040236);
-  final Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
+  //late GoogleMapController mapController;
+  //final LatLng _center = const LatLng(47.497913, 19.040236);
+  //final Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
   List<Event> _events = List.empty();
   final EventService _eventService = EventService();
 
@@ -24,10 +25,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _events = events;
     });
-    showEventsOnMap();
+    //showEventsOnMap();
   }
 
-  void showEventsOnMap() {
+  /*void showEventsOnMap() {
     for (int i = 0; i < _events.length; i++) {
       MarkerId markerId = MarkerId(i.toString());
       _markers[markerId] = Marker(
@@ -36,25 +37,25 @@ class _MyAppState extends State<MyApp> {
         infoWindow: InfoWindow(title: _events[i].name, snippet: ''),
       );
     }
-  }
+  }*/
 
-  @override
+  /*@override
   void initState() {
     super.initState();
     getTodaysEvents();
-  }
+  }*/
 
-  void _onMapCreated(GoogleMapController controller) {
+  /*void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    /*setState(() {
+    setState(() {
       MarkerId markerId = const MarkerId('0');
       _markers[markerId] = Marker(
         markerId: markerId,
         position: const LatLng(45.521563, -122.677433),
         infoWindow: const InfoWindow(title: 'Portland', snippet: '*'),
       );
-    });*/
-  }
+    });
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -64,14 +65,8 @@ class _MyAppState extends State<MyApp> {
         useMaterial3: true,
         colorSchemeSeed: Colors.green[700],
       ),
-      home: Scaffold(
-        body: GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
-            markers: Set<Marker>.of(_markers.values)),
+      home: const Scaffold(
+        body: Map(),
       ),
     );
   }
